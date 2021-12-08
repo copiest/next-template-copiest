34e01bc805260f8efea217e783b71a10c0504193
olaf <olaf@olafui-MacBookAir.local>
2021-12-09 00:31:45 +0900

    feat: setup next

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import { Global } from '@emotion/react'
+
+import globalStyles from '@components/shared/globalStyles'
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Global styles={globalStyles} />
+            <Component {...pageProps} />
+        </>
+    )
+     
+}
+
+export default App 
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import { MESSAGE } from '@constants'
+
+function IndexPage() {
+    return <Container>{MESSAGE}</Container>
+}
+
+const Container = styled.div``
+
+export default IndexPage
