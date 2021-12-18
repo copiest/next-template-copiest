617f25f4b8fac0025f7150ba4d7b9540c5aa7bca
olaf <olaf@olafui-MacBookAir.local>
2021-12-18 22:29:47 +0900

    feat: recoil root 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from 'next/app'
+import {
+  RecoilRoot,
+} from 'recoil';
+
 import { Global } from '@emotion/react'
 import globalStyles from '@components/shared/globalStyles'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global styles={globalStyles} />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Global styles={globalStyles} />
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   )
 }
