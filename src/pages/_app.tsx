2d506e457a2471032f4ae9e646a388a72d4545d8
olaf <olaf@olafui-MacBookAir.local>
2021-12-19 12:45:26 +0900

    feat: react-query 를 추가합니다

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,28 @@
 import { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import { Global } from '@emotion/react'
 import globalStyles from '@components/shared/globalStyles'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      notifyOnChangeProps: 'tracked',
+      keepPreviousData: true,
+      staleTime: 5_000,
+    },
+  },
+})
+
 function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Global styles={globalStyles} />
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Global styles={globalStyles} />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
   )
 }
 
