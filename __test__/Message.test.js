b60e91a471e2bdeee78bfe20e3ee1e2ad204096a
olaf <olaf@olafui-MacBookAir.local>
2021-12-19 12:27:59 +0900

    feat: jest plugin 추가

--- __test__/Message.test.js
+++ __test__/Message.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import IndexPage from '@pages/index'
 import { MESSAGE } from '@constants'
 
-describe('Message', () => {
+describe('Message', async () => {
   it('1205 텍스트가 노출된다', () => {
     render(<IndexPage />)
 
